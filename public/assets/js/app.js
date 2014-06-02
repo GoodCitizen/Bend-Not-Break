12b72acbbf625bb62b63f7abd6e1bf5dbdbd09ec
John Peele <john@wearegoodcitizen.com>
2014-06-02 19:58:10 -0400

    Add jquery to close inactive mobile nav accordions

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,10 +1,19 @@
 (function ($) {
   // Mobile nav accordion
   $('.mobile-nav .nav-title').on('click',function(){
+    var $this = $(this),
+        $navList = $this.next('.nav-list');
+
+    // Close any other open .nav-lists
+    $('.mobile-nav .nav-title').not($this).removeClass('active');
+    $('.nav-list').not($navList).removeClass('active');
+
+    // Toggle the active class on .nav-titles and .nav-lists
     if (Modernizr.mq('(max-width: 480px)')) {
-      $(this).toggleClass('active');
-      $(this).next('.nav-list').toggleClass('active');
-      return false;
+      $this.toggleClass('active');
+      $navList.toggleClass('active');
     }
+
+    return false;
   });
 }(jQuery));
