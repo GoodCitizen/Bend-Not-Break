db415d1c42dfc99a06bca07e3d5c4fb83a6d478b
John Peele <john@wearegoodcitizen.com>
2014-06-10 08:19:07 -0400

    Tweaks to style and responsive issues

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -9,9 +9,9 @@
   });
 
   $("#more-less-options-button").click(function() {
-       var txt = $("#extra-options").is(':visible') ? 'more options' : 'less options';
-       $("#more-less-options-button").text(txt);
-       $("#extra-options").slideToggle();
+    var txt = $("#extra-options").is(':visible') ? 'more options' : 'less options';
+    $("#more-less-options-button").text(txt);
+    $("#extra-options").slideToggle();
   });
 
   // Mobile nav accordion
